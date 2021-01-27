feaeb2f5913aa3c28894ed49d90d3f0281f5fe14
George <55986112+Plutarh@users.noreply.github.com>
2021-01-27 17:21:48 +0300

    Project

---
+++ Assets/Scripts/SimpleMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMover : MonoBehaviour
+{
+    public Vector3 moveDir;
+    public Vector3 rotateDir;
+
+    public GameObject rotateObject;
+
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+        rotateObject.transform.Rotate(rotateDir * rotateSpeed * Time.deltaTime);
+    }
+}
